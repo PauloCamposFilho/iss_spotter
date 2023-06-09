299c1de238aad01bc7154dea6ad049f630162567
Paulo Campos Filho <pezaum@gmail.com>
2023-06-08 20:19:48 -0700

    Added error handling to index2.js

--- index2.js
+++ index2.js
@@ -5,4 +5,5 @@ nextISSTimesForMyLocation()
     for (let passOver of passTimes) {
       console.log("Next pass at", new Date(passOver.risetime * 1000).toString());
     }
-  });
+  })
+  .catch((error) => console.log("It didn't work:", error.message));
